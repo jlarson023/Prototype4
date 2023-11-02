4e97e59dae394ebb64b292102d8793074ccae5d1
jlarson023 <larsontjonathan@gmail.com>
2023-11-02 09:44:47 -0700

    4.4 Done

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,6 +7,7 @@ public class Enemy : MonoBehaviour
     public float speed = 3.0f;
     private Rigidbody enemyRb;
     public GameObject player;
+
     
     // Start is called before the first frame update
     void Start()
@@ -17,9 +18,14 @@ public class Enemy : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {   
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
 
         enemyRb.AddForce(lookDirection * speed);
+
+        if(transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
     }
 }
